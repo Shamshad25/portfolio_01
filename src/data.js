21a0e4ca19d12f32057498e093bdc5a1d07e6dfe
Shamshad Alam <70852128+Shamshad25@users.noreply.github.com>
2022-08-10 01:14:31 +0530

    HahaTalks project added

--- src/data.js
+++ src/data.js
@@ -1,23 +1,30 @@
 import project01 from "../src/img/project01.png";
 import project02 from "../src/img/project02.png";
 import project03 from "../src/img/project03.png";
+import project04 from "../src/img/project04.png";
 
 export const products = [
   {
     id: 1,
-    image: project02,
+    image: project01,
     link: "https://tourista01.netlify.app",
     project: "Tourista",
   },
   {
     id: 2,
-    image: project01,
-    link: "https://soultasty.netlify.app/",
-    project: "SoulTasty",
+    image: project02,
+    link: "https://tourista01.netlify.app",
+    project: "HahaTalks",
   },
   {
     id: 3,
     image: project03,
+    link: "https://soultasty.netlify.app/",
+    project: "SoulTasty",
+  },
+  {
+    id: 4,
+    image: project04,
     link: "https://shamshad.netlify.app/",
     project: "Portfolio",
   },
