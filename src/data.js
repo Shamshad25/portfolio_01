836aaaecbef8db295aa8f0e69cd7e3b2b3b9fd30
Shamshad Alam <70852128+Shamshad25@users.noreply.github.com>
2022-08-30 21:19:58 +0530

    New Project added

--- src/data.js
+++ src/data.js
@@ -2,6 +2,7 @@ import project01 from "../src/img/project01.png";
 import project02 from "../src/img/project02.png";
 import project03 from "../src/img/project03.png";
 import project04 from "../src/img/project04.png";
+import project05 from "../src/img/project05.png";
 
 export const products = [
   {
@@ -13,8 +14,8 @@ export const products = [
   {
     id: 2,
     image: project02,
-    link: "#",
-    project: "HahaTalks",
+    link: "https://expressit01.netlify.app/",
+    project: "ExpressIt",
   },
   {
     id: 3,
@@ -25,6 +26,12 @@ export const products = [
   {
     id: 4,
     image: project04,
+    link: "#",
+    project: "HahaTalks",
+  },
+  {
+    id: 5,
+    image: project05,
     link: "https://shamshad.netlify.app/",
     project: "Portfolio",
   },
