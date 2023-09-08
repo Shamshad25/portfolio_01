2a0b206b87456c67ad7e4f80e7c2bb56a4f02077
Shamshad Alam <70852128+Shamshad25@users.noreply.github.com>
2023-09-09 00:24:40 +0530

    ShoeStore Project added

--- src/data.js
+++ src/data.js
@@ -4,42 +4,49 @@ import project03 from "../src/img/project03.png";
 import project04 from "../src/img/project04.png";
 import project05 from "../src/img/project05.png";
 import project06 from "../src/img/project06.png";
+import project07 from "../src/img/project07.png";
 
 export const products = [
   {
     id: 1,
     image: project01,
-    link: "https://tourista01.netlify.app",
-    project: "Tourista",
+    link: "https://shoestore025.netlify.app/",
+    project: "ShoeStore",
   },
   {
     id: 2,
     image: project02,
-    link: "https://expressit01.netlify.app/",
-    project: "ExpressIt",
+    link: "https://tourista01.netlify.app",
+    project: "Tourista",
   },
   {
     id: 3,
     image: project03,
-    link: "https://soultasty.netlify.app/",
-    project: "SoulTasty",
+    link: "https://expressit01.netlify.app/",
+    project: "ExpressIt",
   },
   {
     id: 4,
     image: project04,
-    link: "#",
-    project: "HahaTalks",
+    link: "https://soultasty.netlify.app/",
+    project: "SoulTasty",
   },
   {
     id: 5,
     image: project05,
-    link: "https://shamshad.netlify.app/",
-    project: "Portfolio",
+    link: "#",
+    project: "HahaTalks",
   },
   {
     id: 6,
     image: project06,
     link: "https://contact-manage01.netlify.app/",
     project: "Contact-Manager & Covid-19-Survey",
+  },
+  {
+    id: 7,
+    image: project07,
+    link: "https://shamshad.netlify.app/",
+    project: "Portfolio",
   },
 ];
